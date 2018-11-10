a39ae5059734c6a8ada37e20432c7c74f7b8aa53
Anton Drobot <anton-drobot@users.noreply.github.com>
2018-11-10 17:10:16 +0300

    chore(version): version 1.0.0-beta.0

---
+++ src/Exceptions/ContainerExceptionInterface.ts
@@ -0,0 +1,6 @@
+import ExceptionInterface from '@banejs/exceptions/ExceptionInterface';
+
+/**
+ * Base interface representing a generic exception in a container.
+ */
+export default interface ContainerExceptionInterface extends ExceptionInterface {}
